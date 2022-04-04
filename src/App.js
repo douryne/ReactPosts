aff87891945d60be1a13b26652c404e361fc772c
douryne <douryne.1st@gmail.com>
2022-04-04 23:13:13 +0500

    posts list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import PostItem from "./components/PostItem";
-import "./styles/App.css";
+import React, { useState } from 'react';
+import PostItem from './components/PostItem';
+import './styles/App.css';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'Description' },
+    { id: 2, title: 'JavaScript', body: 'Description' },
+    { id: 3, title: 'JavaScript', body: 'Description' }
+  ]);
+
   return (
-    <div className="App">
-      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>
+    <div className='App'>
+      <h1 style={{ textAlign: 'center', color: 'white' }}>Posts List</h1>
+      {posts.map(post => 
+        <PostItem post={ post } key={ post.id }/>
+      )}
     </div>
   );
 }
