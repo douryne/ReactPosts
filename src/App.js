3765d7b1a211c003133e891544e01ab1cb5a8f27
douryne <douryne.1st@gmail.com>
2022-04-04 22:38:33 +0500

    PostItem component, base styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import PostItem from "./components/PostItem";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
+      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>
     </div>
   );
 }
