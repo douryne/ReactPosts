c2057656150b9fa6cbec24b9051d47016621e5db
douryne <douryne.1st@gmail.com>
2022-04-09 19:43:35 +0500

    AppRouter component

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Posts from '../pages/Posts/Posts';
-import About from '../pages/About';
-import Error from '../pages/Error';
+import { BrowserRouter } from 'react-router-dom';
 import MyNavbar from '../components/UI/Navbar/MyNavbar';
+import AppRouter from '../components/AppRouter';
 
 function App() {
 return (
     <div className='App'>
       <BrowserRouter>
       <MyNavbar />
-        <Routes>
-          <Route path='/' element={<Posts />} />
-          <Route path='about' element={<About />} />
-          <Route path='*' element={<Error />} />
-        </Routes>
+      <AppRouter />
       </BrowserRouter>
     </div>
   );
