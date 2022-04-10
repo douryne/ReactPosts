23b60e057b84ce43246dd58bdab403e00b949411
douryne <douryne.1st@gmail.com>
2022-04-10 16:09:14 +0500

    limit select

--- src/hooks/useFetching.jsx
+++ src/hooks/useFetching.jsx
@@ -4,10 +4,10 @@ const useFetching = (callback) => {
   const [isLoading, setIsLoading]  = useState(true);
   const [error, setError] = useState('');
 
-  const fetching = async (page, posts) => {
+  const fetching = async (page, posts, limit) => {
       try {
       setIsLoading(true);
-      await callback(page, posts);
+      await callback(page, posts, limit);
     } catch (e) {
       setError(e.message);
     } finally {
--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -11,23 +11,28 @@ import MyModal from '../../components/UI/Modal/MyModal';
 import {getPageCount} from '../../utils/pages';
 import classes from './Posts.module.css';
 import { useObserver } from '../../hooks/useObserver';
+import MySelect from '../../components/UI/Select/MySelect';
 
 function Posts() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({searchQuery: '', selectedSort: ''})
   const [modal, setModal] = useState(false);
-  const [limit] = useState(10);
+  const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const [totalPageCount, setTotalPageCount] = useState(1);
   const lastElement = useRef();
 
   const sortedAndFiltered = usePosts(posts, filter.searchQuery, filter.selectedSort);
 
-  const [fetchPosts, isPostsLoading, error] = useFetching(async (page, posts) => {
+  const [fetchPosts, isPostsLoading, error] = useFetching(async (page, posts, limit) => {
     const response = await PostService.getByPage(limit, page);
     const totalPostsCount = response.headers['x-total-count'];
     setTotalPageCount(getPageCount(totalPostsCount, limit));
-    setPosts([...posts, ...response.data]);
+    if(Math.floor(posts.length / limit)  === page - 1) {
+      setPosts([...posts, ...response.data]);
+      return
+    }
+    setPosts([...response.data]);
   })
 
   useObserver(lastElement, page < totalPageCount, isPostsLoading, () => {
@@ -35,9 +40,9 @@ function Posts() {
   });
 
   useEffect(() => {
-    fetchPosts(page, posts);
+    fetchPosts(page, posts, limit);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
+  }, [page, limit]);
 
   const deleteAllPosts = () => {
     setPosts([]);
@@ -66,7 +71,26 @@ function Posts() {
       </MyModal>
 
       {
-        Boolean(posts.length) && <PostFilter filter={filter} setFilter={setFilter}/>
+        Boolean(posts.length) && 
+        <PostFilter filter={filter} setFilter={setFilter}/>
+      }
+      {
+        Boolean(posts.length) &&
+        <MySelect
+          value={limit}
+          onSelectChange={value => 
+            {
+              setLimit(value);
+              setPage(1);
+            }}
+          defaultValue='amount of posts to show'
+          options={[
+            {value: 5, name: '5'},
+            {value: 10, name: '10'},
+            {value: 25, name: '25'},
+            {value: -1, name: 'Show all'},
+          ]}
+        />
       }
       {
         error
